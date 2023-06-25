268816c4510991eb1086dac464d37e415f464b89
Li LEI <130347271+shannon-headmaster@users.noreply.github.com>
2023-06-25 09:17:28 +0800

    queue

---
+++ queue/Common.php
@@ -0,0 +1,18 @@
+<?php
+
+class Common
+{
+    public function getList(){
+        echo 'headmaster-it/queue/Common/getList';
+    }
+
+    public function edit()
+    {
+        echo 'headmaster-it/queue/Common/edit';
+    }
+
+    public function delete()
+    {
+        echo 'headmaster-it/queue/Common/delete';
+    }
+}
